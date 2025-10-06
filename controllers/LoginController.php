aace98c687c70a7f868c3e9f8530354430376e66
Maty1337 <matidavidbuena@gmail.com>
2025-10-06 15:53:55 -0300

    Estructura Inicial

---
+++ controllers/LoginController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Controllers;
+
+
+class LoginController
+{
+    public static function login()
+    {
+        echo "hola";
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
+
+    public static function logout()
+    {
+        echo "hola";
+    }
+
+    public static function crear()
+    {
+        echo "crear";
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
+
+    public static function olvide()
+    {
+        echo "olvide";
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
+    public static function restablecer()
+    {
+        echo "olvide";
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
+
+    public static function mensaje()
+    {
+        echo "olvide";
+    }
+    public static function confirmar()
+    {
+        echo "olvide";
+    }
+}
